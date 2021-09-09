0f23bafb4cf06d8d074c51c812c1b90c8448832a
christina-yun <poncheeyun@yahoo.com>
2021-09-08 17:20:17 -0700

    Got the drinklistitems to render and if you click details, it works

--- app/src/components/DrinkList.js
+++ app/src/components/DrinkList.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import DrinkListItem from './DrinkListItem';
 
+import { connect } from 'react-redux';
+
 const DrinkList = (props) => {
-    // const drinks = 
+    const drinks = props.drinks;
+
     return (
         <div>
             <h4>Drink List</h4>
-            <DrinkListItem />
+            {drinks.map(drink => {
+                return <DrinkListItem key={drink.idDrink} drink={drink}/>
+            })}
         </div>
         
     )
 }
-export default DrinkList;
+const mapStateToProps = (state) => {
+    return ({
+        drinks: state.cocktail.drinks
+    })
+}
+export default connect(mapStateToProps)(DrinkList);
--- app/src/components/DrinkListItem.js
+++ app/src/components/DrinkListItem.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DrinkListItem = (props) => {
+    const { drink } = props;
+    console.log(drink)
     return(
         <div className='drink-card'>
             
-            <img src='#' width='100px' alt='a cocktail'/>
-            <h3>Drink List Item Name</h3>
-
-            {/* Need to come back and link to drink id */}
-            
-            <Link to={`/drinks/    `} className='view'>
+            <img src={drink.strDrinkThumb} width='100px' alt='a cocktail'/>
+            <h3>{drink.strDrink}</h3>
+            <Link to={`/drinks/${drink.idDrink}`} className='view'>
                 <button className='drink-details'>View Details</button>
             </Link>
         </div>
